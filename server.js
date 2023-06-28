1ceeb9bd4846317ad378d502381b2d4f29b05b3f
Samardeep Kajal <samk712003@gmail.com>
2023-06-28 18:37:04 +0530

    Added errorHandler middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const colors = require("colors");
 const connectDB = require("./backend/config/db");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./backend/middleware/errorMiddleware");
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/tasks", require("./backend/routes/tasksRoutes"));
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Server started on port ${port}`));
